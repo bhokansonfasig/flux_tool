d19338fb70247d357af319de1bab46e730c20297
Benjamin Hokanson-Fasig <bhokansonfasig@gmail.com>
2021-01-08 17:33:41 -0600

    Add units to flux_tool imports

--- flux_tool/__init__.py
+++ flux_tool/__init__.py
@@ -1,4 +1,5 @@
 """Tool for calculating sensitivities and neutrino counts for detectors."""
 
-from .model import Model
 from .fluxes import veff_to_aeff, flux_sensitivity, neutrino_count
+from .model import Model
+from . import units
